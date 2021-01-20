c91de07734d973d3f5a8735898ae26bc4db5a4da
Danylo Petrakivskyi <35082522+xXxRisingTidexXx@users.noreply.github.com>
2021-01-21 01:05:14 +0200

    Deploy sh (#2)
    * Removed prd deploy from lets
    
    * Added deploy shell script

---
+++ deploy.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+export HIRCUM_TRITICUM_ENV=prd && \
+export HIRCUM_TRITICUM_PORT=80 && \
+docker-compose build --no-cache parser && \
+docker-compose down && \
+docker-compose up -d parser
