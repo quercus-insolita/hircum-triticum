681784444a38ca070d27c96580a74856fd60cb89
xXxRisingTidexXx <danylopetrik@gmail.com>
2021-01-22 23:22:26 +0200

    Added aquamarket list css selector

--- internal/parsing/aquamarket_parser.go
+++ internal/parsing/aquamarket_parser.go
@@ -1,18 +1,36 @@
 package parsing
 
 import (
+	log "github.com/sirupsen/logrus"
+	"golang.org/x/net/html"
 	"net/http"
 	"time"
 )
 
 func MewAquamarketParser() Parser {
-	return &aquamarketParser{&http.Client{Timeout: time.Second * 10}}
+	return &aquamarketParser{&helper{&http.Client{Timeout: time.Second * 10}}}
 }
 
 type aquamarketParser struct {
-	client *http.Client
+	helper *helper
 }
 
 func (p *aquamarketParser) ParseBuckwheats() ([]Buckwheat, error) {
-	return make([]Buckwheat, 0), nil
+	document, err := p.helper.readDocument("https://aquamarket.ua/uk/1099-grechka")
+	if err != nil {
+		return nil, err
+	}
+	buckwheats := make([]Buckwheat, 0)
+	for _, node := range document.Find("div.product:not(.product-not-available)").Nodes {
+		if buckwheat, err := p.parseBuckwheat(node); err != nil {
+			log.WithField("url", buckwheat.URL).Error(err)
+		} else {
+			buckwheats = append(buckwheats, buckwheat)
+		}
+	}
+	return buckwheats, nil
+}
+
+func (p *aquamarketParser) parseBuckwheat(node *html.Node) (Buckwheat, error) {
+	return Buckwheat{}, nil
 }
