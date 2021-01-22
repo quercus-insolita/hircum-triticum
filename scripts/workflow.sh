a589a72f9958dafc9ffb31ddb9bcb83000584f3c
xXxRisingTidexXx <danylopetrik@gmail.com>
2021-01-23 01:25:44 +0200

    Added more parser containers in production

--- scripts/workflow.sh
+++ scripts/workflow.sh
@@ -1,5 +1,5 @@
 mkdir -p logs && \
 docker-compose -f docker-compose-prd.yml build --no-cache parser && \
 docker-compose -f docker-compose-prd.yml down && \
-docker-compose -f docker-compose-prd.yml up -d parser && \
+docker-compose -f docker-compose-prd.yml up -d parser-auchan parser-aquamarket && \
 docker image prune -f
