8468dde0f9d0d90fa9405b031f17b415249a6608
xXxRisingTidexXx <danylopetrik@gmail.com>
2021-01-23 01:20:35 +0200

    Finished aquamarket parsing

--- internal/parsing/aquamarket_parser.go
+++ internal/parsing/aquamarket_parser.go
@@ -5,15 +5,21 @@ import (
 	"github.com/PuerkitoBio/goquery"
 	log "github.com/sirupsen/logrus"
 	"net/http"
+	"regexp"
+	"strconv"
 	"time"
 )
 
 func MewAquamarketParser() Parser {
-	return &aquamarketParser{&helper{&http.Client{Timeout: time.Second * 10}}}
+	return &aquamarketParser{
+		&helper{&http.Client{Timeout: time.Second * 10}},
+		regexp.MustCompile(`^.*, (\d+ \* )?(\d+) (к?г)(\W.*)?$`),
+	}
 }
 
 type aquamarketParser struct {
 	helper *helper
+	regexp *regexp.Regexp
 }
 
 func (p *aquamarketParser) ParseBuckwheats() ([]Buckwheat, error) {
@@ -52,9 +58,51 @@ func (p *aquamarketParser) parseBuckwheat(selection *goquery.Selection) (Buckwhe
 	if buckwheat.URL == "" {
 		return buckwheat, fmt.Errorf("parsing: parser found no url")
 	}
-	buckwheat.URL = "https://aquamarket.ua" + buckwheat.URL
 	if buckwheat.Title == "" {
 		return buckwheat, fmt.Errorf("parsing: parser found no title")
 	}
-	return buckwheat, nil
+	nodes = selection.Find("span.price.product-price[content]").Nodes
+	if len(nodes) != 1 {
+		return buckwheat, fmt.Errorf("parsing: parser found no price node")
+	}
+	var price string
+	for _, attribute := range nodes[0].Attr {
+		if attribute.Key == "content" {
+			price = attribute.Val
+		}
+	}
+	buckwheat.Price, err = strconv.ParseFloat(price, 64)
+	if err != nil {
+		return buckwheat, fmt.Errorf("parsing: parser failed to parse price, %v", err)
+	}
+	if buckwheat.Price <= 0 {
+		return buckwheat, fmt.Errorf("parsing: parser found non-positive price")
+	}
+	groups := p.regexp.FindStringSubmatch(buckwheat.Title)
+	buckwheat.Mass, err = strconv.ParseFloat(groups[2], 64)
+	if err != nil {
+		return buckwheat, fmt.Errorf("parsing: parser failed to parse right mass, %v", err)
+	}
+	if buckwheat.Mass <= 0 {
+		return buckwheat, fmt.Errorf("parsing: parser found non-positive right mass")
+	}
+	if groups[1] != "" {
+		mass, err := strconv.ParseFloat(groups[1][:len(groups[1])-3], 64)
+		if err != nil {
+			return buckwheat, fmt.Errorf("parsing: parser failed to parse left mass, %v", err)
+		}
+		if mass <= 0 {
+			return buckwheat, fmt.Errorf("parsing: parser found non-positive left mass")
+		}
+		buckwheat.Mass *= mass
+	}
+	switch groups[3] {
+	case "кг":
+		return buckwheat, nil
+	case "г":
+		buckwheat.Mass /= 1000
+		return buckwheat, nil
+	default:
+		return buckwheat, fmt.Errorf("parsing: parser found unknown mass unit")
+	}
 }
