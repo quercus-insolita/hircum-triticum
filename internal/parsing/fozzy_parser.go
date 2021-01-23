440d66004e7977cc31e78bdb33f1b3abfdc0128f
xXxRisingTidexXx <danylopetrik@gmail.com>
2021-01-24 00:57:54 +0200

    Added item loop

--- internal/parsing/fozzy_parser.go
+++ internal/parsing/fozzy_parser.go
@@ -1,6 +1,8 @@
 package parsing
 
 import (
+	"github.com/PuerkitoBio/goquery"
+	log "github.com/sirupsen/logrus"
 	"net/http"
 	"time"
 )
@@ -14,5 +16,22 @@ type fozzyParser struct {
 }
 
 func (p *fozzyParser) ParseBuckwheats() ([]Buckwheat, error) {
-	return []Buckwheat{}, nil
+	document, err := p.helper.readDocument("https://fozzyshop.ua/300143-krupa-grechnevaya")
+	if err != nil {
+		return nil, err
+	}
+	buckwheats := make([]Buckwheat, 0)
+	selection := document.Find("div.js-product-miniature-wrapper:not(.product_grey)")
+	for i := range selection.Nodes {
+		if buckwheat, err := p.parseBuckwheat(selection.Eq(i)); err != nil {
+			log.WithField("url", buckwheat.URL).Error(err)
+		} else {
+			buckwheats = append(buckwheats, buckwheat)
+		}
+	}
+	return buckwheats, nil
+}
+
+func (p *fozzyParser) parseBuckwheat(selection *goquery.Selection) (Buckwheat, error) {
+	return Buckwheat{}, nil
 }
