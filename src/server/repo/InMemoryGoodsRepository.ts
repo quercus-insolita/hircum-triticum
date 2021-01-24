74d3e3d1d709fa79b9a80c15efdeeb7410633a0e
Andrii Pavlenko <andriipav@wix.com>
2021-01-24 22:39:48 +0200

    create InMemoryGoodsRepository

--- src/server/repo/InMemoryGoodsRepository.ts
+++ src/server/repo/InMemoryGoodsRepository.ts
@@ -1,11 +1,58 @@
 import { InternalGood } from '../domain'
 
+type SourceRecord = {
+    readonly lastUpdate: Date
+    readonly goods: InternalGood[]
+}
+
 export class InMemoryGoodsRepository {
-    private readonly goodsDictionary: Record<string, InternalGood> = {}
+    private readonly goodsBySourceDict: Record<string, SourceRecord> = {}
+    private lastUpdate: Date | null = null // the last time this.createOrUpdate or this.bulkCreateOrUpdate was called
 
-    getLatest() {}
+    getLatest(): Record<string, InternalGood[]>
+    getLatest(sourceId: string): InternalGood[]
+    getLatest(sourceId?: string): InternalGood[] | Record<string, InternalGood[]> {
+        if (sourceId !== undefined) {
+            return this.goodsBySourceDict[sourceId].goods
+        }
 
-    createOrUpdate() {}
+        return Object.entries(this.goodsBySourceDict).reduce<
+            Record<string, InternalGood[]>
+        >((acc, [key, value]) => {
+            acc[key] = value.goods
+            return acc
+        }, {})
+    }
 
-    getLastUpdateTimestamp() {}
+    createOrUpdate(sourceId: string, goods: InternalGood[]): Date {
+        this.goodsBySourceDict[sourceId] = {
+            goods,
+            lastUpdate: new Date()
+        }
+
+        this.lastUpdate = this.goodsBySourceDict[sourceId].lastUpdate
+        return this.lastUpdate
+    }
+
+    bulkCreateOrUpdate(data: Record<string, InternalGood[]>): Date {
+        const currentTime = new Date()
+        Object.entries(data).forEach(([sourceId, goods]) => {
+            this.goodsBySourceDict[sourceId] = {
+                goods,
+                lastUpdate: new Date(currentTime)
+            }
+        })
+
+        this.lastUpdate = currentTime
+        return this.lastUpdate
+    }
+
+    getLastUpdateTimestamp(): Date | null
+    getLastUpdateTimestamp(sourceId: string): Date
+    getLastUpdateTimestamp(sourceId?: string): Date | null {
+        if (sourceId !== undefined) {
+            return this.goodsBySourceDict[sourceId].lastUpdate
+        }
+        return this.lastUpdate
+    }
 }
