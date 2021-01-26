9a35ac1e5f9a3a5331c6e1a9c0e339a78f984d5f
Andrii Pavlenko <andriipav@wix.com>
2021-01-26 20:57:38 +0200

    adjust express, npm scripts and docker stuff to serve static

--- src/server/express.ts
+++ src/server/express.ts
@@ -1,6 +1,7 @@
 import express from 'express'
 import cors from 'cors'
 import { parse } from 'qs'
+import * as path from 'path'
 
 import { router as apiRouter } from './routers/api'
 import { createAppContext } from './context'
@@ -14,7 +15,7 @@ export const createExpressApp = (goodsRepository: InMemoryGoodsRepository) => {
 
   app.use(cors())
   app.use(express.json())
-  app.use(express.static('static'))
+  app.use(express.static('src/client/build'))
 
   app.use((req, _res, next) => {
     Object.assign(req, createAppContext(goodsRepository))
@@ -23,7 +24,7 @@ export const createExpressApp = (goodsRepository: InMemoryGoodsRepository) => {
 
   const externalEndpointsRegex = /^\/(?!_)/ // all routes that start from _ are reserved for internal usage
   app.get(externalEndpointsRegex, (_req, res) => {
-    res.status(200).sendFile('index.html', { root: __dirname })
+    res.status(200).sendFile('index.html', { root: path.resolve(__dirname, '/src/client/build') })
   })
 
   app.use('/_api', apiRouter)
